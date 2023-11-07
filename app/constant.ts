6dfc2a11bbef3f71fe7d5ac6ec8bd3b87bda6b9d
tianxiang.ftx <tianxiang.ftx@alibaba-inc.com>
2023-11-07 16:16:17 +0800

    fix

--- app/constant.ts
+++ app/constant.ts
@@ -77,27 +77,19 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const DEFAULT_MODELS = [
   {
+    name: "gpt-4-1106-preview",
+    available: true,
+  },
+  {
+    name: "gpt-4-vision-preview",
+    available: true,
+  },
+  {
     name: "gpt-4",
     available: true,
   },
   {
-    name: "gpt-4-0314",
-    available: true,
-  },
-  {
-    name: "gpt-4-0613",
-    available: true,
-  },
-  {
     name: "gpt-4-32k",
-    available: true,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: true,
-  },
-  {
-    name: "gpt-4-32k-0613",
     available: true,
   },
   {
